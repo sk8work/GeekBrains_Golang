ee57e039eb22b36e4cb6e68dd503073d4d31949f
sk8work <eshikhantsev@gmail.com>
2019-10-21 15:04:34 +0300

    Calc
    Add a calculator

--- Syntax_of_Go/4/3/main.go
+++ Syntax_of_Go/4/3/main.go
@@ -1,8 +1,32 @@
-// Дописать функцию, которая будет выодить справку по калькулятору
-// Она должна вызываться при вводе слова help вместо выражения
-
 package main
 
-func main() {
+import (
+	"Syntax_of_Go/4/3/calculator"
+	"fmt"
+)
 
+func main() {
+	input := ""
+	for {
+		fmt.Print("> ")
+		if _, err := fmt.Scanln(&input); err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		if input == "help" {
+			fmt.Println(calculator.Help())
+			continue
+		}
+
+		if input == "exit" {
+			break
+		}
+
+		if res, err := calculator.Calculate(input); err == nil {
+			fmt.Printf("Результат: %v\n", res)
+		} else {
+			fmt.Println("Не удалось произвести вычисления")
+		}
+	}
 }
